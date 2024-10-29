0b0fab73c2950b6f85ba7ef26eb28bfd0f6b81d7
kimzz14 <kimzz1004@gmail.com>
2024-10-29 19:59:19 +0900

    init

---
+++ run_braker3.sh
@@ -0,0 +1,9 @@
+braker.pl --threads 24 \
+	--species=zebrafish \
+	--useexisting \
+	--genome=ref.fa \
+        --GENEMARK_PATH=./src_packages/GeneMark-ETP/bin \
+        --rnaseq_sets_ids=SRR19088063 \
+        --rnaseq_sets_dirs=/archive/kimzz14/SRA_RAW/INSDC/Nibea_coibor/PRJNA827677 \
+        1> run_braker3.log \
+        2> run_braker3.err
