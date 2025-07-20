4dab33ed48e9d643c35a0f0192d6034700c5bbd4
kimzz14 <kimzz1004@gmail.com>
2025-07-20 03:31:33 +0000

    update

--- 00.export_path.sh
+++ 00.export_path.sh
@@ -1,16 +1,1 @@
-##################################################################################
 source activate braker3_v3.0.8_241029
-##################################################################################
-#conda create -n braker3_v3.0.8_241029
-
-#source activate braker3_v3.0.8_241029
-
-#conda install braker3
-#bioconda/noarch::braker3-3.0.8-hdfd78af_0
-#bioconda/linux-64::augustus-3.5.0-pl5321heb9362c_5
-
-#conda install hisat2
-#bioconda/linux-64::hisat2-2.2.1-hdbdd923_6
-
-#git clone https://github.com/gatech-genemark/GeneMark-ETP.git
-##################################################################################
--- run_braker3.sh
+++ run_braker3.sh
@@ -1,8 +1,10 @@
-braker.pl --threads 24 \
-	--species=zebrafish \
+braker.pl \
+        --threads 16 \
+	--species=arabidopsis_thaliana \
 	--useexisting \
 	--genome=ref.fa \
-        --rnaseq_sets_ids=SRR19088063 \
-        --rnaseq_sets_dirs=/archive/kimzz14/SRA_RAW/INSDC/Nibea_coibor/PRJNA827677 \
+	--prot_seq=protein.fa \
+        --rnaseq_sets_ids=SRR8714016,SRR8759751 \
+        --rnaseq_sets_dirs=/home/kimzz14/SRA_REF/Arabidopsis_thaliana/Athaliana_447/02.braker3_v3.0.8_241029/data/rna-seq \
         1> run_braker3.log \
         2> run_braker3.err
